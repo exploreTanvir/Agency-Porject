fbbd404ff1f2ddd7cec3927436c2ebb78592b8fd
Tanvir_Hossen <tanvirhossen853@gmail.com>
2024-04-14 23:07:06 +0600

    Agency project

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./navbar.css"
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+        <span><h1>Agency</h1></span>
+        <span>button</span>
+    </div>
+  )
+}
+
+export default Navbar
