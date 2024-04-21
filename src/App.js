65c69f6950475dec56b6302dea845377f7de2db8
Tanvir Hossen Raju <103171957+exploreTanvir@users.noreply.github.com>
2024-04-21 22:25:06 +0600

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from 'react'
 import "./app.css"
+import Contact from './components/contact/Contact'
+import Feature from './components/feature/Feature'
+import Footer from './components/footer/Footer'
 import Intro from './components/intro/Intro'
 import Navbar from './components/navbar/Navbar'
+import Price from './components/price/Price'
+import Services from './components/services/Services'
 
 const App = () => {
   return (
     <div className='components'>
       <Navbar/>
       <Intro/>
+      <Feature/>
+      <Services/>
+      <Price/>
+      <Contact/>
+      <Footer/>
     </div>
   )
 }
